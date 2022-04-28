630e050b701b8d6d38c737ad2d60b7d5ab9faba3
Vasily Postnicov <shamaz.mazum@gmail.com>
2022-04-28 13:52:42 +0300

    Add correlation functions for real data

---
+++ scripts&data/plot-real.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+data15581 = np.loadtxt('15581_500x500_seg.dat')
+data15588 = np.loadtxt('15588_500x500_seg.dat')
+data15604 = np.loadtxt('15604_500x500_seg.dat')
+data5479  = np.loadtxt('5479_500x500_seg.dat')
+data5548  = np.loadtxt('5548_500x500_seg.dat')
+data9005  = np.loadtxt('9005-2_500x500_seg.dat')
+
+plt.figure(figsize = (10, 9), dpi = 300)
+plt.rc('font', size = 18)
+plt.plot(data15581[:,0])
+plt.plot(data15588[:,0])
+plt.plot(data15604[:,0])
+plt.plot(data5479[:,0])
+plt.plot(data5548[:,0])
+plt.plot(data9005[:,0])
+plt.xlabel('Correlation length $r$')
+plt.ylabel('Surface-void function $F_{ss}(r)$')
+plt.legend(['Sandstone 1', 'Sandstone 2', 'Sandstone 3', 'Carbonate 1', 'Carbonate 2', 'Carbonate 3'])
+plt.savefig('../images/KIK-ss.png')
